412acee904bc436c99f26357ac9610d6d5716f84
Bas de Ruiter <winkbrace@gmail.com>
2014-05-17 00:49:39 +0200

    database schema created

---
+++ data/kutkabouter.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- Machine: localhost
+-- Genereertijd: 16 mei 2014 om 22:48
+-- Serverversie: 5.6.12-log
+-- PHP-versie: 5.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Databank: `kutkabouter`
+--
+CREATE DATABASE IF NOT EXISTS `kutkabouter` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `kutkabouter`;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `reviews`
+--
+
+CREATE TABLE IF NOT EXISTS `reviews` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text,
+  `longtitude` float NOT NULL,
+  `lattitude` float NOT NULL,
+  `review1` text NOT NULL,
+  `review2` text,
+  `review3` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
