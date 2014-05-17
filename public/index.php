26b1beff16aafcfa2df00380ff11f8b90e22b6bc
Bas de Ruiter <winkbrace@gmail.com>
2014-05-17 15:24:20 +0200

    finished review api

--- public/index.php
+++ public/index.php
@@ -5,15 +5,18 @@ use Kutkabouter\Db;
 require_once realpath(__DIR__ . '/../vendor/autoload.php');
 
 $app = new \Slim\Slim();
-$app->get('/review/:long/:lat', function($long, $lat) {
+$app->response->headers->set('Content-Type', 'application/json');
+
+$app->get('/review/:long/:lat', function($long, $lat) use ($app) {
     $db = new Db();
     $stmt = $db->prepare("select review1, review2, review3 from reviews where longtitude = ? and latitude = ?");
     $stmt->bind_param('dd', $long, $lat);
     $stmt->execute();
     $result = $stmt->get_result();
     $row = $result->fetch_array();
-    $reviews = array_filter(array($row['review1'], $row['review2'], $row['review3']));
-    
-    return json_encode($reviews);
+    $reviews = array_filter($row);
+    $review = $reviews[array_rand($reviews)];
+
+    $app->response->write(json_encode($review));
 });
 $app->run();
