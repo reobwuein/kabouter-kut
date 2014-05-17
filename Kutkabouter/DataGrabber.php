989c054a57ddd74cb129dc1d9494b106616b2863
Bas de Ruiter <winkbrace@gmail.com>
2014-05-17 10:32:01 +0200

    finished importing restaurants and hotels location data

--- Kutkabouter/DataGrabber.php
+++ Kutkabouter/DataGrabber.php
@@ -1,5 +1,6 @@
 <?php namespace Kutkabouter;
 
+use Kutkabouter\Fetchers\GeoFetcher;
 use Kutkabouter\Fetchers\CsvFetcher;
 use Kutkabouter\Db;
 
@@ -20,6 +21,7 @@ class DataGrabber
     public function grabAll()
     {
         $this->grabEtenEnDrinken();
+        $this->grabHotels();
     }
 
     public function grabEtenEnDrinken()
@@ -28,6 +30,7 @@ class DataGrabber
 
         $headers = array_flip($fetcher->getHeaders());
 
+        $count = 0;
         while ($row = $fetcher->next())
         {
             if (empty($row[$headers['Longitude']]) || empty($row[$headers['Latitude']]))
@@ -39,15 +42,53 @@ class DataGrabber
                 'name' => ! empty($row[$headers['Title']]) ? $row[$headers['Title']] : '',
             );
 
-            $stmt = $this->db->prepare("insert into reviews (longtitude, latitude, name) values (?, ?, ?)");
-            $stmt->bind_param('dds', $row['longtitude'], $row['latitude'], $row['name']);
+            $stmt = $this->db->prepare("insert into reviews (longtitude, latitude, name, category) values (?, ?, ?, ?)");
+            $stmt->bind_param('ddss', $row['longtitude'], $row['latitude'], $row['name'], 'Bar/Restaurant');
             $stmt->execute();
+
+            $count++;
+            echo 'added restaurant '.$row['name'].PHP_EOL;
         }
+
+        echo 'Finished importing '.$count.' restaurants'.PHP_EOL.PHP_EOL;
+    }
+
+    public function grabHotels()
+    {
+        $fetcher = new CsvFetcher('http://www.amsterdamopendata.nl/documents/10180/25203/Lijst%20hotels%20MRA%202012.csv', ';');
+        $headers = array_flip($fetcher->getHeaders());
+
+        $gf = new GeoFetcher();
+
+        $count = 0;
+        while ($row = $fetcher->next())
+        {
+            if ($row[$headers['gemeente']] != 'AMSTERDAM')
+                continue;
+
+            $name = $row[$headers['hotel naam in 2012']];
+            $postcode = $row[$headers['postcode']];
+            $geo = $gf->fetch($postcode);
+            $long = $this->convertToNumber($geo['longtitude']);
+            $lat = $this->convertToNumber($geo['latitude']);
+
+            $stars = $row[$headers['sterklasse NHC 2012']];
+            $review1 = ($stars <= 1) ? 'This cheap excuse for a hotel has only '.$stars.' stars' : null;
+
+            $stmt = $this->db->prepare("insert into reviews (longtitude, latitude, name, category, review1) values (?, ?, ?, ?, ?)");
+            $stmt->bind_param('ddsss', $long, $lat, $name, 'Hotel', $review1);
+            $stmt->execute();
+
+            $count++;
+            echo 'added hotel '.$name.PHP_EOL;
+        }
+
+        echo 'Finished importing '.$count.' hotels'.PHP_EOL.PHP_EOL;
     }
 
     protected function convertToNumber($number)
     {
-        $parts = explode(',', $number);
-        return $parts[0] . '.' . substr($parts[1], 0, 3);
+        $number = str_replace(',', '.', $number);
+        return round((float) $number, 3);
     }
 } 
--- public/grab.php
+++ public/grab.php
@@ -1,8 +1,11 @@
 <?php namespace Kutkabouter;
 
+set_time_limit(60 * 60); // 1 hour
+
 require_once realpath(__DIR__ . '/../vendor/autoload.php');
 
 $grabber = new DataGrabber(new Db());
-//$grabber->grabEtenEnDrinken();
+$grabber->grabEtenEnDrinken();
+$grabber->grabHotels();
 
-echo 'Done.';
+echo 'Done.'.PHP_EOL;
